81a1551d379f9d0d6869b44a9cf46adc6a6a5de0
Mahsum Altun <mahsummaltunn@gmail.com>
2022-03-30 19:38:35 +0300

    Know if the object is in the air or on the ground
    The object knows that it is in the air and on the ground, the footsteps
    turn off when in the air, and open when it falls to the ground.

--- Assets/Script/CarMove.cs
+++ Assets/Script/CarMove.cs
@@ -8,141 +8,128 @@ public class CarMove : MonoBehaviour
   //Create the rigidbody and the collider
   public Rigidbody rb;
   private Collider col;
-  
   //Create a variable you can change in inspector windown for speed
   [SerializeField]
   private float speed;
-
   [SerializeField]
   private float smoothRotation;
+  public Rigidbody rbGo;
+  //Create a variable for the PlayerCollider class
+  PlayerCollider m_playerCollider;
+  //Created to tune the sound of an object in the air and on the ground
+  public AudioSource audioSource1, audioSource2, audioSource3;
   
-
-  void OnCollisionStay (Collision other)
-  {
-      if(other.gameObject.tag != "ground")
-      {
-         speed = 0f;
-      }
-      else
-      {
-          speed = 150f;
-      }
-
-  }
   void Start ()
   {
-      //Get the rigidbody and collider
-      rb = GetComponent<Rigidbody>();
-      col = GetComponent<Collider>();
+    //Get the PlayerCollider class
+    m_playerCollider = GetComponent<PlayerCollider>();
+    //Get the collider
+    col = GetComponent<Collider>();
   }
-
-
 
   void Update ()
   {  
-     float dot = Vector3.Dot(transform.up,Vector3.up);
-     if (dot <= -0.5f)
+    //Reset speed when object turns upside down
+    float dot = Vector3.Dot(transform.up,Vector3.up);
+    if (dot <= -0.5f)
+    {
+        speed = 0;
+    }
+     //Is OnCollisionStay on the ground?
+     if (m_playerCollider.IsOnGround)
      {
-         speed = 0;
+         //Moves the object forward
+        if (moveForward)
+        {
+          rb.AddRelativeForce(Vector3.forward * speed); 
+        } 
+        //Moves the object backwards
+        if (moveBackward)
+        {
+          rb.AddRelativeForce(Vector3.back * speed);
+        }
+        //Turns up sound if OnCollisionStay is on the ground
+        audioSource1.volume = 0.11f;
+        audioSource2.volume = 0.11f;
+        audioSource3.volume = 0.11f;
      }
-  
+     else
+    {
+        //OnCollisionStay mutes sound if in mid-air
+        audioSource1.volume = 0f;
+        audioSource2.volume = 0f;
+        audioSource3.volume = 0f;
+    }
      //Get the vehicle's speed
      var velocity = rb.velocity;
      var localVel = transform.InverseTransformDirection(velocity);
 
      if (moveLeft)
      {
-         // Turn the vehicle direction to the left
-         transform.Rotate(Vector3.up, -90 * smoothRotation * Time.deltaTime);
-
-         // Go in the direction the vehicle is facing     
-         if (localVel.z > 0) 
-         {
+        // Turn the vehicle direction to the left
+        transform.Rotate(Vector3.up, -90 * smoothRotation * Time.deltaTime);
+        // Go in the direction the vehicle is facing     
+        if (localVel.z > 0) 
+        {
             rb.velocity = transform.forward * rb.velocity.magnitude;
-         }
+        }
          else
          {
              rb.velocity = -transform.forward * rb.velocity.magnitude;
          }
-     
      }
-
-     
-     
      if (moveRight)
-         {
-            // Turn the vehicle direction to the right
-            transform.Rotate(Vector3.up, 90 * smoothRotation * Time.deltaTime);
-
-            // Go in the direction the vehicle is facing
-            if (localVel.z > 0)
-            {
-               rb.velocity = transform.forward * rb.velocity.magnitude;
-            }
-            else 
-            {
-                rb.velocity = -transform.forward * rb.velocity.magnitude;
-            }
-         
-         }
-
-
-     if (moveForward)
      {
-         rb.AddRelativeForce(Vector3.forward * speed); 
-
-     } 
-
-
-     if (moveBackward)
-     {
-         rb.AddRelativeForce(Vector3.back * speed);
-
+        // Turn the vehicle direction to the right
+        transform.Rotate(Vector3.up, 90 * smoothRotation * Time.deltaTime);
+        // Go in the direction the vehicle is facing
+        if (localVel.z > 0)
+        {
+            rb.velocity = transform.forward * rb.velocity.magnitude;
+        }
+        else 
+        {
+            rb.velocity = -transform.forward * rb.velocity.magnitude;
+        }
      }
+    }
   
-  
-  }
-  
-
-
   public void MoveLeft ()
   {
-      moveLeft = true;
+    moveLeft = true;
   }
-
   public void StopMovingLeft ()
   {
-      moveLeft = false;
+    moveLeft = false;
   }
-
   public void MoveRight ()
   {
-      moveRight = true; 
+    moveRight = true; 
   }
-
   public void StopMovingRight ()
   {
-      moveRight = false;
+    moveRight = false;
   }
-
   public void MoveForward ()
   {
-      moveForward = true;
+    moveForward = true;
+    //OnCollisionStay stops when a rigidbody sleeps Wake rigidbody when asks OnCollisionStay to run
+    rbGo.WakeUp();
   }
-
   public void StopMovingForward ()
   {
-      moveForward = false;
+    moveForward = false;
   }
-
   public void MoveBackward ()
   {
-      moveBackward = true;
+    moveBackward = true;
+    //OnCollisionStay stops when a rigidbody sleeps Wake rigidbody when asks OnCollisionStay to run
+    rbGo.WakeUp();
   }
-
   public void StopMovingBackward ()
   {
-      moveBackward = false;
+    moveBackward = false;
   }
-  
+
+
 }
---
+++ Assets/Script/PlayerCollider.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    private bool m_IsOnGround;
+    private float distToGround;
+ 
+     public bool IsOnGround
+     {
+         get
+         {
+             if (m_IsOnGround)
+             {
+                 m_IsOnGround = false;
+                 return true;
+             }
+             else
+             {
+                 return false;
+             }
+         }
+     }
+     void OnCollisionStay()
+     {
+         //If it touch things, then it's on ground, that's my rule
+         m_IsOnGround = true;
+     }
+}
