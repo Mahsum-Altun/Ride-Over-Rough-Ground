b8850e16d25d832656a3dd537ba678dfc461397e
Mahsum Altun <mahsummaltunn@gmail.com>
2022-05-07 17:28:54 +0300

    Add sound to coin

--- Assets/Script/CollectingCoins.cs
+++ Assets/Script/CollectingCoins.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class CollectingCoins : MonoBehaviour
 {
    public int coins;
+   public AudioSource CoinSource;
+   public AudioClip CoinClip;
 
    public void OnTriggerEnter(Collider Col) 
    {
@@ -11,6 +13,7 @@ public class CollectingCoins : MonoBehaviour
            SaveManager.instance.money += 1;
            SaveManager.instance.Save();
            Destroy(Col.gameObject);
+           CoinSource.PlayOneShot(CoinClip);
        }
    }
 }
